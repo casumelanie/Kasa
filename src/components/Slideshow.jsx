2bf37719f8acec9ba3e0dc11da0bb1aae468de6a
casumelanie <melanie.casu-pro@outlook.fr>
2024-03-22 14:40:18 +0100

    Refacto + ajout mise en page du slider

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,24 +1,18 @@
-import { useGetLodgingById } from '../components/api'
 import arrowRight from '../assets/slideshow-arrow_right.svg'
 import arrowLeft from '../assets/slideshow-arrow_left.svg'
 import { useState } from 'react'
 
-const Slideshow = ({ id, pictures }) => {
-  const { data, isLoading, error } = useGetLodgingById(id)
+const Slideshow = ({ pictures }) => {
   const [activeSlide, setActiveSlide] = useState(0)
   const prevSlide = activeSlide === 0 ? pictures.length - 1 : activeSlide - 1
   const nextSlide = activeSlide === pictures.length - 1 ? 0 : activeSlide + 1
-  console.log(pictures)
 
   return (
     <>
-      {isLoading ? (
-        //chargement
-        <div>Chargement en cours...</div>
-      ) : pictures.length === 1 ? (
+      {pictures.length === 1 ? (
         // une seule image
         <div className="slideshow-container">
-          <img className="picture" src={pictures} alt="test" />
+          <img className="picture" src={pictures} alt="Chambre" />
         </div>
       ) : (
         // plusieurs images
@@ -36,8 +30,12 @@ const Slideshow = ({ id, pictures }) => {
             onClick={() => setActiveSlide(nextSlide)}
           />
           <>
-            <img className="picture" src={pictures[activeSlide]} alt="test" />
-            <span>
+            <img
+              className="picture"
+              src={pictures[activeSlide]}
+              alt="Images de la chambre"
+            />
+            <span className="slide-counter">
               {activeSlide + 1}/{pictures.length}
             </span>
           </>
--- src/pages/Lodging.jsx
+++ src/pages/Lodging.jsx
@@ -6,20 +6,14 @@ import Slideshow from '../components/Slideshow.jsx'
 const Lodging = () => {
   const { id } = useParams()
   const { data, isLoading, error } = useGetLodgingById(id)
-  const pictures = data.pictures
-
   if (error) return <Error404 />
-  //console.log(data)
-  //console.log(pictures)
 
   return (
     <div className="main-container">
       {isLoading ? (
         <div>Chargement en cours...</div>
       ) : (
-        <div className="main-container">
-          <Slideshow id={id} pictures={pictures} />
-        </div>
+        <Slideshow pictures={data?.pictures} />
       )}
     </div>
   )
